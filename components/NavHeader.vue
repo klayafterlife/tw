c1dfbdb0e8a557936a8e935969e3f40492e32d4c
klayafterlife <klayafterlife@gmail.com>
2022-04-19 21:24:20 +0900

    보호색 활성화, 트위터 -> 디스코드

--- components/NavHeader.vue
+++ components/NavHeader.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="header">
-    <a class="col-4 link disable" @click="goToPage('/')">보호색</a>
+    <a class="col-4 link" @click="goToPage('/camouflage')">보호색</a>
     <a class="col-4 link disable" @click="goToPage('/')">낚시</a>
 
     <div class="mt-4">
-      <a class="col-4 link" href="https://twitter.com/blanky_nftdev" target="_blank">트위터</a>
+      <a class="col-4 link" href="https://discord.gg/RtXGG5jxuu" target="_blank">디스코드</a>
       <a class="col-4 link" href="https://opensea.io/collection/tinywhales" target="_blank">오픈씨</a>
       <a class="col-4 link" href="https://klu.bs/pfp/0x2089CFC532195E9568608fD0E8aD7Ab5e4cfDc91" target="_blank">클럽스</a>
     </div>
