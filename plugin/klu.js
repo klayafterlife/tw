d23e34dff1c45e02211b68cd3e60146ebf8f2414
klayafterlife <klayafterlife@gmail.com>
2022-06-05 00:02:25 +0900

    klu 컨트랙트 추가

---
+++ plugin/klu.js
@@ -0,0 +1,30 @@
+export const KLU_ADDR = '0x5F034c71eE5Cc0B4e7688AF8a2B55C32b0EfDE00';
+
+export const KLU_ABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+];
