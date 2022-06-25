86afe3b673800e87c7289678647b7d0cc855be02
klayafterlife <klayafterlife@gmail.com>
2022-06-25 13:28:17 +0900

    주석 제거, 메뉴 이동 시 햄버거 닫힘

--- components/NavHeader.vue
+++ components/NavHeader.vue
@@ -1,14 +1,4 @@
 <template>
-<!-- <a class="col-4 link" @click="goToPage('/')">메인</a>
-    <a class="col-4 link" @click="goToPage('/mint')">포획</a>
-    <a class="col-4 link" @click="goToPage('/camouflage')">보호색</a>
-    <a class="col-4 link" @click="goToPage('/adventure')">탐험</a>
-
-    <div class="mt-4">
-      <a class="col-4 link" href="https://discord.gg/RtXGG5jxuu" target="_blank">디스코드</a>
-      <a class="col-4 link" href="https://opensea.io/collection/tinywhales" target="_blank">오픈씨</a>
-      <a class="col-4 link" href="https://klu.bs/pfp/0x2089CFC532195E9568608fD0E8aD7Ab5e4cfDc91" target="_blank">클럽스</a>
-    </div> -->
   <nav class="navbar">
     <div :class="['navbar__logo', { 'active': navOpen }]" @click="goToPage('/')">
       <img src="/img/whale.png" alt="logo">
@@ -64,6 +54,7 @@ export default {
 
     goToPage(page) {
       this.$router.push(page);
+      this.navOpen = false;
     }
   }
 }
